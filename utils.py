ae497c5f983e540617de8f6140c99c9fb7be4d27
Gabriel Nunes <94002497+Gabriel7763@users.noreply.github.com>
2023-12-29 16:07:00 -0300

    criando funcoes
    Implementando as funções do redis

--- utils.py
+++ utils.py
@@ -1,39 +1,127 @@
+import redis
+
+
+def gera_id():
+    try:
+        connection = conectar()
+        chave = connection.get('chave')
+
+        if chave:
+            chave = connection.incr('chave')
+            return chave
+        else:
+            connection.set('chave', 1)
+            return 1
+    except redis.exceptions.ConnectionError as e:
+        print(f'Não foi possível gerar a chave: {e}')
+
+
 def conectar():
     """
     Função para conectar ao servidor
     """
-    print('Conectando ao servidor...')
+    connection = redis.Redis(host='localhost', port=6379)
+    return connection
 
-def desconectar():
+
+def desconectar(connection):
     """ 
     Função para desconectar do servidor.
     """
-    print('Desconectando do servidor...')
+    connection.connection_pool.disconnect()
 
 
 def listar():
     """
     Função para listar os produtos
     """
-    print('Listando produtos...')
+    connection = conectar()
+
+    try:
+        dados = connection.keys(pattern='produtos:*')
+        if len(dados) > 0:
+            print('Listando produtos...')
+            print('--------------------')
+            for chave in dados:
+                #traz todos os dados de produto de acordo com a chave
+                produto = connection.hgetall(chave)
+                print(f"ID: {str(chave, 'utf-8', 'ingore')}")
+                print(f"Produto: {str(produto[b'nome'], 'utf-8', 'ingore')}")
+                print(f"Preço: {str(produto[b'preco'], 'utf-8', 'ingore')}")
+                print(f"Estoque: {str(produto[b'estoque'], 'utf-8', 'ingore')}")
+                print('-----------------------------')
+        else:
+            print('Não existem produtos cadastrados')
+    except redis.exceptions.ConnectionError as e:
+        print(f'Não foi possível listar os produtos. {e}')
+    desconectar(connection)
 
 def inserir():
     """
     Função para inserir um produto
     """  
-    print('Inserindo produto...')
+    connection = conectar()
+
+    nome = input('Informe o nome do produto: ')
+    preco = float(input('Informe o preço: '))
+    estoque = int(input('Informe o estoque: '))
+
+    produto = {"nome": nome, "preco": preco, "estoque": estoque}
+    chave = f'produtos:{gera_id()}'
+
+    try:
+        res = connection.hmset(chave, produto)
+        if res:
+            print(f'O produto {nome} foi inserido com sucesso!')
+        else:
+            print(f'Não foi possível inserir o produto')
+    except redis.exceptions.ConnectionError as e:
+        print(f'Não foi possível inserir o produto: {e}')
+
 
 def atualizar():
     """
     Função para atualizar um produto
     """
-    print('Atualizando produto...')
+    connection = conectar()
+
+    chave = input('Informe a chave do produto: ')
+    if not connection.exists(chave):
+        print('Essa chave não existe')
+    else:
+        nome = input('Informe o nome do produto: ')
+        preco = float(input('Informe o preço: '))
+        estoque = int(input('Informe o estoque: '))
+
+        produto = {"nome": nome, "preco": preco, "estoque": estoque}
+
+        try:
+            res = connection.hmset(chave, produto)
+            if res:
+                print(f'O produto {nome} foi atualizado com sucesso!')
+        except redis.exceptions.ConnectionError as e:
+            print(f'Não foi possível atualizar o produto: {e}')
+        desconectar(connection)
+    desconectar(connection)
+
 
 def deletar():
     """
     Função para deletar um produto
     """  
-    print('Deletando produto...')
+    connection = conectar()
+
+    chave = input('Informe a chave do produto: ')
+
+    try:
+        res = connection.delete(chave)
+        if res == 1:
+            print('O produto foi deletado com sucesso')
+        else:
+            print('Não existe produto com a chave informada.')
+    except:
+        print('Não existe produto com a chave informada')
+    desconectar(connection)
 
 def menu():
     """
